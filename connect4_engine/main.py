679accf0eb5168b0ceee9bb5130cc48e35e6988a
Ariel Shumacher <ariegg210@gmail.com>
2026-03-10 14:17:15 +0200

    icky

--- connect4_engine/main.py
+++ connect4_engine/main.py
@@ -3,12 +3,14 @@ import serial
 from connect4_engine.game import Connect4Game
 from connect4_engine.hardware.robot import RobotCommunicator
 from connect4_engine.hardware.arduino import ArduinoCommunicator
+from connect4_engine.hardware.mock import RobotDummy
 
 
 class Main:
     def __init__(self):
         self.arduino = ArduinoCommunicator(ser=serial.Serial("COM9", 115200))
-        self.robot = RobotCommunicator(com_port="COM11", pump=self.arduino)
+        # self.robot = RobotCommunicator(com_port="COM11", pump=self.arduino)
+        self.robot = RobotDummy()
         self.game = Connect4Game(arduino=self.arduino, robot=self.robot, player_starts=False)
     
     def play(self):
